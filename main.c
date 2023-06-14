3e24d18bfbc32c636f82ca2eade7e4609053d90e
Elizaveta Pavlushina <elpavlushina@mail.ru>
2023-06-14 19:06:21 +0700

    Change of the third byte

--- main.c
+++ main.c
@@ -3,6 +3,7 @@ int main(){
 	
 	int a = 0xDDCCAABB;
 	unsigned char c;
+	
 	c = (a & 0x000000FF);
 	printf("first byte: %x\n", c);
 	c = ((a >> 8) & 0xFF);
@@ -10,6 +11,17 @@ int main(){
 	c = ((a >> 16) & 0xFF);
         printf("third byte: %x\n", c);
 	c = ((a >> 24) & 0xFF);
+        printf("fourth byte: %x\n\n", c);
+	
+	a = (a & 0xFF00FFFF);
+	a = (a | 0x00AB0000);
+	
+	c = (a & 0x000000FF);
+        printf("first byte: %x\n", c);
+        c = ((a >> 8) & 0xFF);
+        printf("second byte: %x\n", c);
+        c = ((a >> 16) & 0xFF);
+        printf("third byte: %x\n", c);
+        c = ((a >> 24) & 0xFF);
         printf("fourth byte: %x\n", c);
-
 }
